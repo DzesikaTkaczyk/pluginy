9fb80756e07c894f768a58e277a4e774137a498b
Dzesika Tkaczyk <dzesika.tkaczyk@gmail.com>
2019-07-14 20:54:43 +0200

    change carousel 14.2

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
+'use strict';
+(function(){ 
+	var carouselCell = document.getElementById('carousel-cell-js').innerHTML;
+	Mustache.parse(carouselCell);
+	var dataList = '';
+	for(var i = 0; i < dataTable.length; i++){
+		console.log(dataTable);
+		dataList += Mustache.render(carouselCell, dataTable[i]);
+	}
+	console.log(dataList)
+	var carouselJs = document.getElementById('carousel-js');
+	
+	carouselJs.innerHTML = dataList;
+
+
 var elem = document.querySelector('.carousel');
 var flkty = new Flickity( elem, {
   // options
@@ -18,4 +33,6 @@ var resetButton = document.querySelector('.restart-button');
 resetButton.addEventListener('click', function(){
 	flkty.select(0)
 })
+
+})();
 
