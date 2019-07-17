e8b8ffb0637921986aa6842075bbed72ef5f6388
Dzesika Tkaczyk <dzesika.tkaczyk@gmail.com>
2019-07-17 22:41:48 +0200

    change picture after clikc on marker

--- js/script.js
+++ js/script.js
@@ -7,46 +7,52 @@
 		console.log(dataTable);
 		dataList += Mustache.render(carouselCell, dataTable[i]);
 	}
-	console.log(dataList)
+	console.log(dataList);
 	var carouselJs = document.getElementById('carousel-js');
 	
 	carouselJs.innerHTML = dataList;
 
-
-var elem = document.querySelector('.carousel');
-var flkty = new Flickity( elem, {
-  freeScroll: true,
-  wrapAround: true,
-  contain: true,
-  hash: true,
-});
-
-var progressBar = document.querySelector('.progress-bar')
-
-flkty.on( 'scroll', function( progress ) {
-  progress = Math.max( 0, Math.min( 1, progress ) );
-  progressBar.style.width = progress * 100 + '%';
-});
-
-var resetButton = document.querySelector('.restart-button');
-resetButton.addEventListener('click', function(){
-	flkty.select(0)
-})
-
-window.initMap = function() {
-	var coords = '';
-	var mapMarker = dataTable[0].coords;
-	var map = new google.maps.Map(document.getElementById('map'), {
-		zoom: 15, center: mapMarker
+	var elem = document.querySelector('.carousel');
+	var flkty = new Flickity( elem, {
+		freeScroll: true,
+		wrapAround: true,
+		contain: true,
+		hash: true,
 	});
-	for(var i = 0; i < dataTable.length; i++){
-		mapMarker = dataTable[i].coords;
-		var marker = new google.maps.Marker({
-			position: mapMarker, map: map
-		});	
-	}
- 
-}
+
+	var progressBar = document.querySelector('.progress-bar')
+
+	flkty.on( 'scroll', function( progress ) {
+		progress = Math.max( 0, Math.min( 1, progress ) );
+		progressBar.style.width = progress * 100 + '%';
+	});
+
+	var resetButton = document.querySelector('.restart-button');
+
+	resetButton.addEventListener('click', function(){
+		flkty.select(0);
+	});
+
+	window.initMap = function() {
+		var mapMarker = dataTable[0].coords;
+		var map = new google.maps.Map(document.getElementById('map'), {
+			zoom: 13, center: mapMarker
+		});
+		var marker;
+		for(var i = 0; i < dataTable.length; i++){
+			mapMarker = dataTable[i].coords;
+			marker = new google.maps.Marker({
+				position: mapMarker, map: map
+			});
+
+			function galleryScroll(x){
+				flkty.select(x);
+			}
+
+			google.maps.event.addListener(marker, 'click', galleryScroll.bind(this, i));
+		};
+	};
+
 
 })();
 
