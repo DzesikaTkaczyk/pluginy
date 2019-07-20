1412e0fc52650430cae8ed94905555ac55fbb644
DzesikaTkaczyk <50298851+DzesikaTkaczyk@users.noreply.github.com>
2019-07-20 19:55:30 +0200

    add event listener

--- js/script.js
+++ js/script.js
@@ -1,21 +1,23 @@
-var elem = document.querySelector('.carousel');
-var flkty = new Flickity( elem, {
-  // options
-  freeScroll: true,
-  wrapAround: true,
-  contain: true,
-  hash: true,
+window.addEventListener('DOMContentLoaded', (event) => {
+	var elem = document.querySelector('.carousel');
+	var flkty = new Flickity( elem, {
+	  // options
+	  freeScroll: true,
+	  wrapAround: true,
+	  contain: true,
+	  hash: true,
+	});
+
+	var progressBar = document.querySelector('.progress-bar')
+
+	flkty.on( 'scroll', function( progress ) {
+	  progress = Math.max( 0, Math.min( 1, progress ) );
+	  progressBar.style.width = progress * 100 + '%';
+	});
+
+	var resetButton = document.querySelector('.restart-button');
+	resetButton.addEventListener('click', function(){
+		flkty.select(0)
+	})
 });
-
-var progressBar = document.querySelector('.progress-bar')
-
-flkty.on( 'scroll', function( progress ) {
-  progress = Math.max( 0, Math.min( 1, progress ) );
-  progressBar.style.width = progress * 100 + '%';
-});
-
-var resetButton = document.querySelector('.restart-button');
-resetButton.addEventListener('click', function(){
-	flkty.select(0)
-})
 
